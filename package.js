3c60d8c88cbdc656d068bba0a2c30bf8484c4deb
unknown <rwatts@SV-WS-DEV-116.220OFFICE.local>
2015-11-05 08:05:04 +0000

    bump v0.1.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'rwatts:orionjs-dashboard',
-	version: '0.1.1',
+	version: '0.1.2',
 	// Brief, one-line summary of the package.
 	summary: 'Adds a Dashboard and Widgets to Orion.',
 	// URL to the Git repository containing the source code for this package.
